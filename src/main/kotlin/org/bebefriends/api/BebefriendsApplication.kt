2910682a019acc5d1e90e4a8ec03bbb02fd49fcd
jsween5723 <jsween5723@gmail.com>
2024-09-30 06:18:58 +0900

    build: 프로젝트 초기화

---
+++ src/main/kotlin/org/bebefriends/api/BebefriendsApplication.kt
@@ -0,0 +1,11 @@
+package org.bebefriends.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BebefriendsApplication
+
+fun main(args: Array<String>) {
+    runApplication<BebefriendsApplication>(*args)
+}
---
+++ src/test/kotlin/org/bebefriends/api/BebefriendsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bebefriends.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BebefriendsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
