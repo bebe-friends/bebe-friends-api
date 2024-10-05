74a9e7f6e4eb19eba04e2ee5ac4dd17b098e27f8
jsween5723 <jsween5723@gmail.com>
2024-10-05 12:23:54 +0900

    feat(user): 닉네임 중복체크 추가

--- src/main/kotlin/org/bebefriends/api/users/UserController.kt
+++ src/main/kotlin/org/bebefriends/api/users/UserController.kt
@@ -6,20 +6,24 @@ import org.bebefriends.core.domain.user.TermAgreements
 import org.bebefriends.core.domain.user.UserInfo
 import org.bebefriends.core.domain.user.UserService
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/users")
 class UserController(private val userService: UserService) {
     @PostMapping
-    fun createUser(@AuthenticationPrincipal uid: String, @RequestBody request: CreateUserRequest) {
+    fun createUser(@AuthenticationPrincipal uid: String, @RequestBody request: CreateUserRequestBody) {
         userService.createUser(request.toUserInfo(), request.toTermAgreements(), uid)
     }
+
+    @GetMapping("check")
+    fun checkFormData(query: CheckUserDataQuery) {
+        if (query.nickname != null) {
+            userService.validateNickname(query.nickname)
+        }
+    }
 }
-data class CreateUserRequest(@NotNull val nickname: String, @NotNull val terms: Set<TermAgreement>) {
+data class CreateUserRequestBody(@NotNull val nickname: String, @NotNull val terms: Set<TermAgreement>) {
     fun toUserInfo():UserInfo {
         return UserInfo(nickname)
     }
@@ -27,3 +31,5 @@ data class CreateUserRequest(@NotNull val nickname: String, @NotNull val terms: 
         return TermAgreements(terms)
     }
 }
+
+data class CheckUserDataQuery(val nickname: String?)
